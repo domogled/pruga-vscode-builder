93754d1f597da47c636ba9a101bb944d07ba897a
Петр Болф <petr.bolf@domogled.com>
2016-11-07 13:56:01 +0100

    beta to use

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let config = vscode.workspace.getConfiguration('pruga')
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -11,17 +13,50 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "pruga-vscode-builder" is now active!');
 
+    const projectPath: string = vscode.workspace.rootPath
+    let terminal = vscode.window.createTerminal('pruga-terminal') 
+    terminal.show()
+    terminal.sendText(`cd ${projectPath}`)
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable_build_all = vscode.commands.registerCommand('pruga.build', () => {
+        terminal.sendText(`npm run clean`)
+        terminal.sendText(`npm run compile-typescript`)
+        terminal.sendText(`npm run compile-php`)
+        terminal.sendText(`npm run compile-elm`)
+        terminal.sendText(`cp ./src/favicon.ico ./build/dev/favicon.ico`)
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Пруга: project is build');
     });
 
-    context.subscriptions.push(disposable);
+    let disposable_compile_file = vscode.commands.registerTextEditorCommand('pruga.compile', (editor, editorEdit) => {
+        const document = editor.document
+
+        document.save()
+            .then(
+                () => {
+                    terminal.sendText(`npm run compile-${document.languageId}`)
+                }
+            )
+            /*.then(
+                () => {
+                    vscode.commands.executeCommand('vscode.previewHtml', `http://localhost/${path.basename(rootPath)}`, vscode.ViewColumn.Two)
+                    // vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two)
+                    .then(
+                        s => console.log('Пруга конец.'),
+                        // then error
+                        vscode.window.showErrorMessage
+                        )
+                }
+            )*/
+
+        vscode.window.showInformationMessage(`Пруга: project file is compiled`);
+    });
+
+    context.subscriptions.push(disposable_build_all);
+    context.subscriptions.push(disposable_compile_file);
 }
 
 // this method is called when your extension is deactivated
