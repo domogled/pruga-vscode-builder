9008b3c93020bd6859973fe00b264e3ac1f7a9d5
Петр Болф <petr.bolf@domogled.com>
2016-11-08 01:44:06 +0100

    configuration with pruga config file

--- src/extension.ts
+++ src/extension.ts
@@ -2,33 +2,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-let config = vscode.workspace.getConfiguration('pruga')
-
-const SHOW_INFORMATION_MESSAGE = config["showInformationMessage"] || false
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const rootPath = vscode.workspace.rootPath;
+    const config = require(path.join(rootPath, 'pruga.js'))
+    const SHOW_INFORMATION_MESSAGE = config["showInformationMessage"]
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "pruga-vscode-builder" is now active!');
 
-    const projectPath: string = vscode.workspace.rootPath
     let terminal = vscode.window.createTerminal('pruga-terminal') 
     terminal.show()
-    terminal.sendText(`cd ${projectPath}`)
+    terminal.sendText(`cd ${rootPath}`)
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable_build_all = vscode.commands.registerCommand('pruga.build', () => {
-        terminal.sendText(`npm run clean`)
-        terminal.sendText(`npm run compile-typescript`)
-        terminal.sendText(`npm run compile-php`)
-        terminal.sendText(`npm run compile-elm`)
-        terminal.sendText(`cp ./src/favicon.ico ./build/dev/favicon.ico`)
+        terminal.sendText(config.builder.clean)
+
+        const compile = config.builder.compile
+
+        for(let item in compile) {
+            terminal.sendText(compile[item])
+        }
+
+        const copy = config.builder.copy
+
+        for(let item in copy) {
+            terminal.sendText(copy[item])
+        }
 
         if(SHOW_INFORMATION_MESSAGE) {
             vscode.window.showInformationMessage('Пруга builder: project is build')
@@ -42,14 +49,19 @@ export function activate(context: vscode.ExtensionContext) {
         document.save()
             .then(
                 () => {
-                    terminal.sendText(`npm run compile-${document.languageId}`)
+                    const command = config.builder.compile[document.languageId]
+                    if(command) {
+                        terminal.sendText(command)
+                    }
+                    else{
+                        vscode.window.showErrorMessage(`Command for ${document.languageId} is not defined.`)
+                    }
+                    
                 }
             )
             .then(
                 () => {
                     vscode.commands.executeCommand('pruga.preview', vscode.ViewColumn.Two)
-                    // vscode.commands.executeCommand('vscode.previewHtml', `http://localhost/${path.basename(rootPath)}`, vscode.ViewColumn.Two)
-                    // vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two)
                     .then(
                         success => console.log(`Пруга builder executeCommand pruga.preview OK ${success}.`),
                         // then error
